1a6b3a2ba8777cf5d8db157e171955c0f4d0a4d6
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-03-04 17:57:38 +0200

    Add spec to trap memleak in Volume mount/unmount

--- spec/glusterfs/stress/memleak_spec.rb
+++ spec/glusterfs/stress/memleak_spec.rb
@@ -33,6 +33,26 @@ module GlusterFS
                          .mount(GFS_SERVER_HOST, GFS_SERVER_PORT) }
     let(:root_dir) { Directory.new(volume, '/memtest-root') }
 
+    # Mem leak here! Only run when MEM_ITERATIONS is provided.
+    if ENV['MEM_ITERATIONS']
+      context 'mount / unmount volume' do
+        specify do
+          puts "Volume Iterations: #{iterations}"
+          mem_size_before = GlusterFS.mem_size
+          iterations.times do |i|
+            vol = Volume.new(GFS_VOLUME)
+            vol.mount(GFS_SERVER_HOST, GFS_SERVER_PORT)
+            vol.unmount
+            puts i
+          end
+          mem_size = GlusterFS.mem_size
+          puts "Mem growth: #{mem_size - mem_size_before}"
+          Mass.print if ENV['MEM_PRINT']
+          (mem_size - mem_size_before).should be < 10240
+        end
+      end
+    end
+
     context 'create many directories' do
       before { root_dir.create }
       after { root_dir.delete }
--- spec/glusterfs/stress/performance_spec.rb
+++ spec/glusterfs/stress/performance_spec.rb
@@ -5,7 +5,7 @@ module GlusterFS
 
   describe 'performance' do
     let(:file_mount_point) { ENV['GFS_MOUNT_DIR'] || '/mnt/dist-volume' }
-    let(:iterations) { (ENV['PERF_ITERATIONS'] || 3000).to_i }
+    let(:iterations) { (ENV['PERF_ITERATIONS'] || 100).to_i }
     let!(:random_blob) do
       '0' * 1048576 * 5 # 5MB
     end
