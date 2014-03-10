589622445203d4cce4899851759df918a7387c7e
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2014-03-10 07:48:39 +0200

    Add GlusterFS::Stat#to_hash

--- lib/glusterfs/stat.rb
+++ lib/glusterfs/stat.rb
@@ -15,4 +15,25 @@ class GlusterFS::Stat < FFI::Struct
          :st_mtimesec,  :ulong,
          :st_ctime,     :ulong,
          :st_ctimesec,  :ulong
+
+  def to_hash
+    {
+      :st_dev      => self[:st_dev],
+      :st_ino      => self[:st_ino],
+      :st_nlink    => self[:st_nlink],
+      :st_mode     => self[:st_mode],
+      :st_uid      => self[:st_uid],
+      :st_gid      => self[:st_gid],
+      :st_rdev     => self[:st_rdev],
+      :st_size     => self[:st_size],
+      :st_blksize  => self[:st_blksize],
+      :st_blocks   => self[:st_blocks],
+      :st_atime    => self[:st_atime],
+      :st_atimesec => self[:st_atimesec],
+      :st_mtime    => self[:st_mtime],
+      :st_mtimesec => self[:st_mtimesec],
+      :st_ctime    => self[:st_ctime],
+      :st_ctimesec => self[:st_ctimesec],
+    }
+  end
 end
